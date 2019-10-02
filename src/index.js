8408f14febd116276bb51af851cc05e071dc9ae4
Eugenia Shibkova <shibkova91@gmail.com>
2019-10-02 19:31:12 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 module.exports = function multiply(first, second) {
-  // your solution
+  const firstNum = first.split('').reverse();
+  const secondNum = second.split('').reverse();
+  const stack = [];
+
+  for (let i = 0; i < first.length; i++) {
+    for (let j = 0; j < second.length; j++) {      
+      let mul = firstNum[i] * secondNum[j];
+      stack[i + j] = (stack[i + j]) ? stack[i + j] + mul : mul;
+    }
+  }  
+
+  for (let i = 0; i < stack.length; i++) {
+    let num = stack[i] % 10;
+    let move = Math.floor(stack[i] / 10);
+    stack[i] = num;
+
+    if (stack[i + 1]) {
+      stack[i + 1] = stack[i + 1] + move;
+    } else if (move > 0) {
+      stack[i + 1] = move;
+    } 
+  }  
+  
+  return stack.reverse().join('');
 }
